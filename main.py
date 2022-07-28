1f0f63d00099949a7eb85e0803ad6239d597a1ce
VaibhavGaneriwala <vaibhav@ganeriwala.com>
2022-07-28 16:02:41 +0530

    Main File

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is a simple example of a QR Code Generator Python script that can be run from the command line.
+# Copyright (c) 2022 Vaibhav Ganeriwala
+
+# import the qrcode module
+import qrcode
